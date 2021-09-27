b703caf7e140421a9868abb7e0c4f31d13db7d13
pureworldnew <pureworldnew@gmail.com>
2021-09-27 00:03:20 -0400

    project update for structure

--- project/frontend/src/components/HorizontalTab/index.js
+++ project/frontend/src/components/HorizontalTab/index.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const HorizontalTab = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <div className="horizontalTab">
       <ul className="horizontalTab__wrapper">
-        {tabs.map(tab => (
+        {tabs.map((tab) => (
           <li
-            key={tab + "tab"}
+            key={tab + 'tab'}
             onClick={() => setActiveTab(tab)}
-            className={activeTab === tab ? "tab tab--active" : "tab"}
-          >
+            className={activeTab === tab ? 'tab tab--active' : 'tab'}>
             {tab}
           </li>
         ))}
